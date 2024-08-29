131f4a76a7156f03ab35e600f2b6b595c50284e3
Alejandro Parias <alejandroparmo@gmail.com>
2024-08-29 04:59:48 -0500

    WIP: Modificación de inyección de dependencia en el controller para que no use @Autowired sino private final

--- billing/src/main/java/com/algorian/springboot/billing/controller/InvoiceRestController.java
+++ billing/src/main/java/com/algorian/springboot/billing/controller/InvoiceRestController.java
@@ -4,7 +4,6 @@ import com.algorian.springboot.billing.entities.Invoice;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import com.algorian.springboot.billing.respository.InvoiceRepository;
 import java.util.Optional;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
 /**
@@ -21,22 +21,22 @@ import org.springframework.http.HttpStatus;
  * @author Alejo0147
  */
 
+@RequiredArgsConstructor
 
 @RestController
 @RequestMapping("/billing")
 public class InvoiceRestController {
     
-    @Autowired
-    InvoiceRepository billingRepository;
+    private final InvoiceRepository _billingRepository;
     
     @GetMapping()
     public List<Invoice> list() {
-        return billingRepository.findAll();
+        return _billingRepository.findAll();
     }
     
     @GetMapping("/{id}")
     public ResponseEntity<?>  get(@PathVariable long id) {
-          Optional<Invoice> invoice = billingRepository.findById(id);
+          Optional<Invoice> invoice = _billingRepository.findById(id);
         if (invoice.isPresent()) {
             return new ResponseEntity<>(invoice.get(), HttpStatus.OK);
         } else {
@@ -51,17 +51,17 @@ public class InvoiceRestController {
     
     @PostMapping
     public ResponseEntity<?> post(@RequestBody Invoice input) {
-        Invoice save = billingRepository.save(input);
+        Invoice save = _billingRepository.save(input);
         return ResponseEntity.ok(save);
     }
     
     @DeleteMapping("/{id}")
     public ResponseEntity<?> delete(@PathVariable String id) {
-         Optional<Invoice> dto = billingRepository.findById(Long.valueOf(id));
+         Optional<Invoice> dto = _billingRepository.findById(Long.valueOf(id));
         if (!dto.isPresent()) {
             return ResponseEntity.notFound().build();
         }
-        billingRepository.delete(dto.get());
+        _billingRepository.delete(dto.get());
         return ResponseEntity.ok().build();
     }
     
